05d0b59c3498c34660f59883b74dfeee7be07e81
Rhys Fernandes <rkfernandes@gmail.com>
2014-09-17 20:27:51 -0400

    update problem 34

--- completed/Problem0034.py
+++ completed/Problem0034.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python
+#!/usr/local/bin/pypy
 
 import math
 
 answer = []
 for n in range(3, 10000000):
-    num = map(lambda x: int(x), str(n)) #array of digits of type int
-    sum_of_factorials = sum(map(lambda num: math.factorial(num), num))
+    sum_of_factorials = sum(map(lambda x: math.factorial(int(x)), str(n)))
     if(sum_of_factorials == n):
         answer.append(sum_of_factorials)
 
