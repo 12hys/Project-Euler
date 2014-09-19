01927b2ae48ba850520c5bd1e5fae83cb72044c7
Rhys <rkfernandes@gmail.com>
2014-09-19 10:51:41 -0400

    Update Problem0092.py

---
+++ Problem0092.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/pypy
+
+def sum_of_digit_squares(num):
+    return sum(map(lambda x: int(x)**2, str(num)))
+
+answers = []
+for num in range(2, 10000001):
+    next_number = sum_of_digit_squares(num)
+
+    while next_number != 89 and next_number != 1:
+        next_number = sum_of_digit_squares(next_number)
+
+    if next_number == 89:
+        answers.append(num)
+
+print len(answers)
