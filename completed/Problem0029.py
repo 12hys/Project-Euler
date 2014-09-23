2cfd9c06a1da6ab619c56bbb1cf531122e5e6652
Rhys Fernandes <rkfernandes@gmail.com>
2014-09-22 21:42:30 -0400

    problem 29 is much faster

--- completed/Problem0029.py
+++ completed/Problem0029.py
@@ -1,12 +1,22 @@
 #!/usr/local/bin/pypy
 
-a = b = range(2, 101)
+import cProfile
 
-answer = []
-for aa in a:
-    for bb in b:
-        ans = pow(aa, bb)
-        if(ans not in answer):
-            answer.append(ans)
+def method_one():
+    numbers = range(2, 101)
+    answer = []
+    for aa in numbers:
+        for bb in numbers:
+            ans = pow(aa, bb)
+            if(ans not in answer):
+                answer.append(ans)
 
-print len(answer)
+    print "Answer: %s" % (len(answer))
+
+def method_two():
+    numbers = range(2, 101)
+    answer = len(list(set([pow(a, b) for a in numbers for b in numbers])))
+    print "Answer: %s" % (answer)
+
+cProfile.run('method_one()')
+cProfile.run('method_two()')
