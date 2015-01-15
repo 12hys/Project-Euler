5fa3c9736824ac1977354c87805a124fda0b24a2
12hys <rkfernandes@gmail.com>
2015-01-14 23:39:13 -0500

    updated problem 53

--- completed/Problem0053.py
+++ completed/Problem0053.py
@@ -1,14 +1,8 @@
 #!/usr/bin/pypy
 
-import math
+from math import factorial
 
-f = math.factorial
-nCr = lambda n, r: f(n) / f(r) / f(n-r)
+nCr = lambda n, r: factorial(n) / factorial(r) / factorial(n-r)
+ncr_list = [nCr(n, r) for n in range(1, 101) for r in range(1, n+1)]
+print len([i for i in ncr_list if i > 1000000])
 
-ans = 0
-for n in range(1, 101):
-	for r in range(1, n+1):
-		if nCr(n, r) > 1000000:
-			ans = ans + 1
-
-print ans
