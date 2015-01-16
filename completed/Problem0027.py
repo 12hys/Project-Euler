547f41e8b9d6653f981e9eceb4ec8872752bcf11
Rhys Fernandes <rkfernandes@gmail.com>
2015-01-16 13:04:33 -0500

    solved problem 27

---
+++ completed/Problem0027.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/pypy
+
+import itertools
+import euler_lib as lib
+import operator
+
+quad = lambda n_range, a, b: [n**2 + a*n + b for n in n_range]
+
+a_b = range(-999, 1000)
+sieve = lib.eratosthenes_sieve(1000000)
+store = {}
+
+for a in a_b:
+    for b in a_b:
+        store[a, b] = 0
+
+        for ctr in itertools.count(2, 1):
+            n_range = range(0, ctr)
+            values = quad(n_range, a, b)
+
+            if all(lib.is_prime(i, sieve) for i in values):
+                continue
+            else:
+                store[a, b] = len(n_range) - 1
+                break
+
+values = max(store.iteritems(), key=operator.itemgetter(1))[0]
+
+print values[0] * values[1]
