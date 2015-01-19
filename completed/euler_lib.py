868a10d1bc7daf0c3ac7f21f8bdc419b7e2667e8
Rhys Fernandes <rkfernandes@gmail.com>
2015-01-19 13:23:57 -0500

    problem 41

--- completed/euler_lib.py
+++ completed/euler_lib.py
@@ -10,6 +10,44 @@ increment = lambda n: n + 1
 def prime(a):
     return not (a < 2 or any(a % x == 0 for x in range(2, int(a ** 0.5) + 1)))
 
+# Sieve of Eratosthenes
+# Code by David Eppstein, UC Irvine, 28 Feb 2002
+# http://code.activestate.com/recipes/117119/
+
+def gen_primes():
+    """ Generate an infinite sequence of prime numbers.
+    """
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+    # indefinitely, but only as long as required by the current
+    # number being tested.
+    #
+    D = {}  
+
+    # The running integer that's checked for primeness
+    q = 2  
+
+    while True:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+            # 
+            yield q        
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next 
+            # multiples of its witnesses to prepare for larger
+            # numbers
+            # 
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+
 def digit_count(n):
     if n > 0:
         return int(math.log10(n)) + 1
@@ -18,6 +56,13 @@ def digit_count(n):
         return 1
 
     return int(math.log10(-n)) + 2
+
+def is_pandigital_return_number(number):
+    digits = sorted(get_digits(number))
+    compare = range(1, len(digits) + 1)
+
+    if cmp(digits, compare) == 0:
+        return number
 
 def is_pandigital(number):
     digits = sorted(get_digits(number))
