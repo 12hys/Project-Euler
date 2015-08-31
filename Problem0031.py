58e1bae666ce50f1a9138c415aae8c467e9b38a1
Rhys Fernandes <rhys.fernandes@salesforce.com>
2015-08-31 14:14:36 -0400

    problem 33 and 31

---
+++ Problem0031.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+count = 0
+
+for two_pounds in range(200, -1, -200):
+	for one_pound in range(two_pounds, -1, -100):
+		for fifty_pence in range(one_pound, -1, -50):
+			for twenty_pence in range(fifty_pence, -1, -20):
+				for ten_pence in range(twenty_pence, -1, -10):
+					for five_pence in range(ten_pence, -1, -5):
+						for two_pence in range(five_pence, -1, -2):
+							count += 1
+		
+print count
--- Problem0033.py
+++ Problem0033.py
@@ -7,4 +7,17 @@ for num in range(1, 100):
         num_digits = lib.get_digits(num)
         den_digits = lib.get_digits(den)
 
-        
+
+
+'''
+#!/usr/bin/python
+
+from fractions import Fraction
+
+numerator = range(10, 100)
+denominator = range(10, 100)
+
+cross_product = [Fraction(x, y) for x in numerator for y in denominator]
+
+print cross_product
+'''
