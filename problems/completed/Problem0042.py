0e9e47afb40348141ccded854d260c526e89e62d
Rhys Fernandes <rkfernandes@gmail.com>
2015-09-24 21:59:06 -0400

    clean up file name in problem 42

--- problems/completed/Problem0042.py
+++ problems/completed/Problem0042.py
@@ -1,11 +1,14 @@
 import csv
+import os
+
+file_path = os.path.join(os.path.dirname(__file__), '..', 'euler_lib/p042_words.txt')
 
 get_num = lambda word: sum(["-ABCDEFGHIJKLMNOPQRSTUVWXYZ".index(char) for char in word])
 tri = lambda n: n * (n + 1) / 2
 
 tri_list = [tri(n) for n in range(1, 100)]
 
-with open('p042_words.txt', 'r') as text_file:
+with open(file_path, 'r') as text_file:
     rows = csv.reader(text_file, delimiter=',', quotechar='"')
     words = [word for row in rows for word in row]
 
