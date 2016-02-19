49a9de80fa1553d0819b71040fccfd98502abd75
Rhys Fernandes <rhys.fernandes@salesforce.com>
2016-02-18 19:38:43 -0500

    refactor problem 18

--- problems/completed/Problem0018.py
+++ problems/completed/Problem0018.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 file_path = os.path.join(os.path.dirname(__file__), '..', 'euler_lib/triangle-18.txt')
 
@@ -11,64 +10,24 @@ for i, value in enumerate(triangle):
 
 file.close()
 
-sum_triangle = []
+sum_triangle = [triangle.pop(0)[0]]
 triangle_size = len(triangle) - 1
-for i, current_row in enumerate(triangle):
-    if i == triangle_size:
-        break
 
-    temp = []
-    next_row = triangle[i + 1]
-    for j, value in enumerate(current_row):
-        if j == 0:
-            temp.append(value + next_row[0])
-            temp.append(value + next_row[1])
-        elif j == 1:
-            print current_row
-            print next_row
-            print temp
-            sys.exit(0)
+for row_index, next_row in enumerate(triangle):
+    sum_temp = []
+    for col_index, value in enumerate(sum_triangle):
+        end_index = len(sum_triangle) - 1
+        if col_index == 0:
+            sum_temp.append(next_row[0] + value)
+            sum_temp.append(next_row[1] + value)
+        elif col_index == end_index:
+            next_row_end_index = len(next_row) - 1
+            sum_temp[col_index] = max(sum_temp[col_index], value + next_row[next_row_end_index - 1])
+            sum_temp.append(value + next_row[next_row_end_index])
         else:
-            temp[j] = max(temp[j], value + next_row[j])
-            temp.append(value + next_row[j + 1])
-            #comparitor = next_row[j + 1]
-            #value_one = value + comparitor
-            #value_two = current_row[j + 1] + comparitor
-            #temp.append(max(value_one, value_two))
-    sum_triangle = temp
+            sum_temp[col_index] = max(sum_temp[col_index], value + next_row[col_index])
+            sum_temp.append(value + next_row[col_index + 1])
+
+    sum_triangle = sum_temp
 
 print max(sum_triangle)
-
-'''
-listOfNumbers = []
-
-for i in range(len(stringsOfNumbers)):
-    stringsOfNumbers[i] = str(stringsOfNumbers[i]).rstrip('\n')
-    listOfNumbers.append(tuple(str(stringsOfNumbers[i]).split(' ')))
-
-# convert strings of numbers to int
-listOfNumbers = [map(int, x) for x in listOfNumbers]
-
-sumOfTriangle = []
-for currentRow in range(len(listOfNumbers)):
-    if(currentRow == 0):
-        sumOfTriangle = listOfNumbers[currentRow]
-    else:
-        currentRowTemp = listOfNumbers[currentRow]
-        for currentColumnIndex in range(len(currentRowTemp)):
-            if(currentColumnIndex == 0):  # first number in triangle
-                currentRowTemp[currentColumnIndex] = sumOfTriangle[0] + currentRowTemp[currentColumnIndex]
-            # last number in the triangle
-            elif (currentColumnIndex == len(currentRowTemp) - 1):
-                currentRowTemp[currentColumnIndex] = sumOfTriangle[len(sumOfTriangle) - 1] + currentRowTemp[currentColumnIndex]
-            else:  # everything in between
-                value1 = currentRowTemp[currentColumnIndex] + sumOfTriangle[currentColumnIndex - 1]
-                value2 = currentRowTemp[currentColumnIndex] + sumOfTriangle[currentColumnIndex]
-                if(value1 > value2):
-                    currentRowTemp[currentColumnIndex] = value1
-                else:
-                    currentRowTemp[currentColumnIndex] = value2
-        sumOfTriangle = currentRowTemp
-
-print max(sumOfTriangle)
-'''
